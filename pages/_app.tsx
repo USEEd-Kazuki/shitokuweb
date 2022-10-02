228d21cd323cb099ae9cda4dc093ec69ae7bc27f
USEEd-Kazuki <kazuone0321@icloud.com>
2022-10-02 13:54:35 +0900

    deploy

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,11 @@
-import { useEffect } from 'react';
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import adobeLoader from "../../adobeLoader";
 import Footer from '../Components/Footer';
 
 
 
 
 function MyApp({ Component, pageProps }: AppProps) {
-  useEffect(() => {
-    if (process.browser) adobeLoader(document);
-  }, [])
   return (
   <>
   <Component {...pageProps} />
