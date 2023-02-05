a1eaa5c827928dfe5679ab829be97480b2c25dc6
USEEd-Kazuki <kazuone0321@icloud.com>
2023-02-05 14:34:16 +0900

    mod

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,6 +11,10 @@ const Home: NextPage = () => {
     <title>進徳海運　株式会社</title>
     <link rel="icon" href="/faviocon.ico" />
     <meta name='viewport' content='width=device-width, initial-scale=1.0, viewport-fit=cover' />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="description" content="船舶貸渡業、東広島に拠点を置く進徳海運株式会社（SHINTOKU KAIUN CO., LTD.）のホームページ。会社情報、採用情報、サービスを掲載" />
+    <meta name="robots" content="noindex,nofollow" />
+    <meta name="keywords" content="Shintoku,進徳海運,船舶,船主,シップオーナー,船舶貸渡業,ship, owner, 広島, japan, finance, chemical"></meta>
   </Head>
   <div className='homeMainbody'>
     <Hero />
