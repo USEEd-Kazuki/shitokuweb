e2a00c990b96653a2eaa9ca5b53c4aa8e0ccad80
USEEd-Kazuki <kazuone0321@icloud.com>
2023-03-14 21:32:14 +0900

    mod

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,6 +10,7 @@ const Home: NextPage = () => {
   <Head>
     <link rel="canonical" href="https://www.shintokukk.co.jp/" />
     <link rel="preconnect" href="https://www.shintokukk.co.jp/"/>
+    <link rel="dns-prefetch" href="https://www.shintokukk.co.jp/" />
     <title>進徳海運株式会社</title>
     <meta property="title" content="進徳海運株式会社" key="title" />
     <link rel="icon" href="/faviocon.ico" />
