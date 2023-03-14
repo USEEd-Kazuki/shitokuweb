9779aafa383f9cd46535acb24d61e84650f161ce
USEEd-Kazuki <kazuone0321@icloud.com>
2023-03-14 10:47:22 +0900

    mod

--- Components/Loading.tsx
+++ Components/Loading.tsx
@@ -19,7 +19,7 @@ const Loading = () => {
     <meta name='viewport' content='width=device-width, initial-scale=1.0, viewport-fit=cover' />
     <meta  name='robots' content='all'/>
     <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="og:description" content="東広島に拠点を置く進徳海運株式会社のホームページです。主に船舶貸渡業を行なっています。会社情報、採用情報、サービスを掲載しています。" />
+    <meta name="description" content="東広島に拠点を置く進徳海運株式会社のホームページです。主に船舶貸渡業を行なっています。会社情報、採用情報、サービスを掲載しています。" />
     <meta name="keywords" content="進徳海運,Shintoku、船舶,船主,シップオーナー,船舶貸渡業,ship, owner, 広島, japan, finance, chemical"></meta>
     </Head>
     <div className="LoadingContent">
