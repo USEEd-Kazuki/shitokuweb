eec7bc4800209929c0c27d0b76491aa37c19eaca
USEEd-Kazuki <kazuone0321@icloud.com>
2023-03-14 10:52:22 +0900

    mod

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,16 +8,17 @@ const Home: NextPage = () => {
   return (
   <>
   <Head>
+    <html lang='ja'/>
     <link rel="canonical" href="https://www.shintokukk.co.jp/" />
     <link rel="preconnect" href="https://www.shintokukk.co.jp/"/>
     <link rel="dns-prefetch" href="https://www.shintokukk.co.jp/"/>
     <title>進徳海運株式会社</title>
     <meta property="og:title" content="進徳海運株式会社" key="title" />
     <link rel="icon" href="/faviocon.ico" />
-    <meta name="og:content-language" content="ja" />
-    <meta name="og:local" content="ja-JP"/>
-    <meta name='og:url' content="https://www.shintokukk.co.jp/"/>
-    <meta name='og:site_name' content='進徳海運株式会社'/>
+    <meta name="content-language" content="ja" />
+    <meta name="local" content="ja-JP"/>
+    <meta name='url' content="https://www.shintokukk.co.jp/"/>
+    <meta name='site_name' content='進徳海運株式会社'/>
     <meta name="google-site-verification" content="PgfRjyZftxIn0m4VL5mAmQJngXhFNyExy3mzlhU9nww" />
     <meta name='viewport' content='width=device-width, initial-scale=1.0, viewport-fit=cover' />
     <meta name='robots' content='all'/>
