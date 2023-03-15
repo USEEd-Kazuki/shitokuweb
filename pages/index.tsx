a8272e5194cdbbb03689ede45a95fba752e544c4
USEEd-Kazuki <kazuone0321@icloud.com>
2023-03-15 17:07:49 +0900

    mod

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,6 +9,9 @@ const Home: NextPage = () => {
   <>
   <Head>
     <title>進徳海運株式会社</title>
+    <link rel="canonical" href="https://www.shintokukk.co.jp/" />
+    <link rel="preconnect" href="https://www.shintokukk.co.jp/"/>
+    <link rel="dns-prefetch" href="https://www.shintokukk.co.jp/" />
     <meta property="title" content="進徳海運株式会社" key="title" />
     <meta name="description" content="東広島に拠点を置く進徳海運株式会社のホームページです。主に船舶貸渡業を行なっています。会社情報、採用情報、サービスを掲載しています。" />
     <meta name="keywords" content="進徳海運,Shintoku、船舶,船主,シップオーナー,船舶貸渡業,ship, owner, 広島, japan, finance, chemical"></meta>
